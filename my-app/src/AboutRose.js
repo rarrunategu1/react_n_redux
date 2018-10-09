17b5efbf3fdbde500fdd4f47b4dd4b0db1eabab0
Rose Parker <parker.rose@ymail.com>
2018-10-09 02:47:21 +0000

    Map through array in app.js, store the jsx in a variable to output the entire jsx list through React

--- my-app/src/AboutRose.js
+++ my-app/src/AboutRose.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 
 class AboutRose extends Component{
     render(){
-        //console.log(this.props);
-        const {name, age, planet} = this.props; //this is destructuring which stores each one in a variable called name, age and planet and now the code below is reusable
+        const {about} = this.props; //this is destructuring which stores each one in a variable called name, age and planet and now the code below is reusable
+        const aboutList = about.map(about =>{
+            return (
+            <div className="about" key={about.id}>
+            <div>Name: {about.name}</div>
+            <div>Age: {about.age}</div>
+            <div>Planet: {about.planet}</div>
+            </div>
+                );
+        });
+        
         return(
-            <div className="about">
-            <div>Name: {name}</div>
-            <div>Age: {age}</div>
-            <div>Planet: {planet}</div>
+            <div className="about-list">
+            {aboutList}
             </div>
             );
     }
