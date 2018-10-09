27e42ede41f6b0e0917754ecc3970b29ed19522a
Rose Parker <parker.rose@ymail.com>
2018-10-09 02:18:28 +0000

    destructuring and changing hard code to dynamic code so that it's reusable

--- my-app/src/AboutRose.js
+++ my-app/src/AboutRose.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class AboutRose extends Component{
     render(){
+        //console.log(this.props);
+        const {name, age, planet} = this.props; //this is destructuring which stores each one in a variable called name, age and planet and now the code below is reusable
         return(
             <div className="about">
-            <div>Name: Rose</div>
-            <div>Age: 45</div>
-            <div>Planet: Mars</div>
+            <div>Name: {name}</div>
+            <div>Age: {age}</div>
+            <div>Planet: {planet}</div>
             </div>
             );
     }
