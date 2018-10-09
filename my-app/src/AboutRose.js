ceb3b7b2f4923dcfe7b8b07b15eabac1f511e9e2
Rose Parker <parker.rose@ymail.com>
2018-10-09 03:35:50 +0000

    two different options for conditional statements - Ternary Operator and If statement

--- my-app/src/AboutRose.js
+++ my-app/src/AboutRose.js
@@ -1,19 +1,36 @@
 import React from 'react';
 
 const AboutRose = ({about}) => { //destructuring is happening directly in the brackets
-const aboutList = about.map(about =>{
-            return (
-            <div className="about" key={about.id}>
-            <div>Name: {about.name}</div>
-            <div>Age: {about.age}</div>
-            <div>Planet: {about.planet}</div>
-            </div>
-                );
-        });
-        
+// const aboutList = about.map(about =>{
+//     if(about.age > 20){
+//               return (
+//             <div className="about" key={about.id}>
+//             <div>Name: {about.name}</div>
+//             <div>Age: {about.age}</div>
+//             <div>Planet: {about.planet}</div>
+//             </div>
+//                 );
+//     } else {
+//         return null;
+//     }
+//         });
+     
+       
         return(
             <div className="about-list">
-            {aboutList}
+            {
+            about.map(about => {
+            return about.age > 20 ? (
+             <div className="about" key={about.id}>
+              <div>Name: {about.name}</div>
+              <div>Age: {about.age}</div>
+              <div>Planet: {about.planet}</div>
+             </div>
+            ) : null;
+        })
+     }
+                
+            
             </div>
             );
     };
