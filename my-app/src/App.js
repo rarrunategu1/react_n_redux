2620b891384b275fb2590562e51019ab22c75e5b
Rose Parker <parker.rose@ymail.com>
2018-10-09 02:41:45 +0000

    created an array to cycle through and output in the view, included unique id's for each object in the array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,19 @@ import AboutRose from './AboutRose';
 
 
 class App extends Component {
+  state = {
+    about: [
+      {name: 'Rose', age: 45, planet: 'Mars', id: 1},
+      {name: 'Jane', age: 56, planet: 'Jupiter', id: 2},
+      {name: 'Joe', age: 25, planet: 'Saturn', id: 3}
+      ]
+  }
   render() {
     return (
       <div className="App">
         <h1>My React app!</h1>
         <p>Welcome</p>
-        <AboutRose name="Rose" age="45" planet="mars" />
-        <AboutRose name="Jane" age="72" planet="venus" />
-      
+        <AboutRose about={this.state.about}/>
       </div>
     );
   }
