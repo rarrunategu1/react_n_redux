7a002df036370ca0e1864f9f0d558923e82ee50c
Rose Parker <parker.rose@ymail.com>
2018-10-09 01:56:56 +0000

    A new component file to next into the React root component

---
+++ my-app/src/AboutRose.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AboutRose extends Component{
+    render(){
+        return(
+            <div className="about">
+            <div>Name: Rose</div>
+            <div>Age: 45</div>
+            <div>Planet: Mars</div>
+            </div>
+            );
+    }
+}
+
+export default AboutRose;
