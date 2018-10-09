8b800aafb3797ffcea0490c01d43f815ca37dac4
Rose Parker <parker.rose@ymail.com>
2018-10-09 01:58:07 +0000

    imported new component file AboutRose.js and nested within the root component so it's rendered in the view

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import AboutRose from './AboutRose';
 
 
 class App extends Component {
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <h1>My React app!</h1>
         <p>Welcome</p>
+        <AboutRose />
       </div>
     );
   }
