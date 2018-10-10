3342f491b9809d7e8683abe7d4630821f40fc656
Rose Parker <parker.rose@ymail.com>
2018-10-10 02:16:11 +0000

    Imported AddPerson and nexted it within the html in the render method

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import AboutRose from './AboutRose';
-
+import AddPerson from './AddPerson';
 
 class App extends Component {
   state = {
@@ -16,6 +16,7 @@ class App extends Component {
         <h1>My React app!</h1>
         <p>Welcome</p>
         <AboutRose about={this.state.about}/>
+        <AddPerson />
       </div>
     );
   }
