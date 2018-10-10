a9c5e1a440e441be73c2769f509eb101f238b217
Rose Parker <parker.rose@ymail.com>
2018-10-10 03:47:25 +0000

    changed back to if statement, included the new deletePerson function from App.js to use for deleting a person in the AboutRose UI component.  Also added a button to perform a delete function when clicked

--- my-app/src/AboutRose.js
+++ my-app/src/AboutRose.js
@@ -1,34 +1,25 @@
 import React from 'react';
 
-const AboutRose = ({about}) => { //destructuring is happening directly in the brackets
-// const aboutList = about.map(about =>{
-//     if(about.age > 20){
-//               return (
-//             <div className="about" key={about.id}>
-//             <div>Name: {about.name}</div>
-//             <div>Age: {about.age}</div>
-//             <div>Planet: {about.planet}</div>
-//             </div>
-//                 );
-//     } else {
-//         return null;
-//     }
-//         });
+const AboutRose = ({about, deletePerson}) => { //destructuring is happening directly in the brackets
+const aboutList = about.map(about =>{
+    if(about.age > 20){
+              return (
+            <div className="about" key={about.id}>
+            <div>Name: {about.name}</div>
+            <div>Age: {about.age}</div>
+            <div>Planet: {about.planet}</div>
+            <button onClick={() => {deletePerson(about.id)}}>Delete person</button>
+            </div>
+                );
+    } else {
+        return null;
+    }
+        });
      
        
         return(
             <div className="about-list">
-            {
-            about.map(about => {
-            return about.age > 20 ? (
-             <div className="about" key={about.id}>
-              <div>Name: {about.name}</div>
-              <div>Age: {about.age}</div>
-              <div>Planet: {about.planet}</div>
-             </div>
-            ) : null;
-        })
-     }
+            {aboutList }
                 
             
             </div>
