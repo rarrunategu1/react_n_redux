b3ad7f0e83a0bbe166e1433f07429496a21e7e7c
Rose Parker <parker.rose@ymail.com>
2018-10-12 03:03:57 +0000

    Navbar component that returns a navbar

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -5,14 +5,14 @@ const Navbar = () => {
         <nav className = "nav-wrapper red darken-3">
             <div className ="container">
             <a className="brand-logo">React Routing</a>
-            <ul>
+            <ul className="right">
             <li><a href="/">Home</a></li>
-            <li><a href="/">Home</a></li>
-            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
             </ul>
             </div>
         </nav>
-        )
-}
+        );
+};
 
 export default Navbar;
