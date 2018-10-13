391c9b4720309105a1bbb50d9f28090b22a69a4c
Rose Parker <parker.rose@ymail.com>
2018-10-13 21:42:16 +0000

    imported a logo in my files and rendered the image on my home page within my JSON object returns

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import CareerDevs from '../careerdevs.png';
 
 class Home extends Component { 
     state = { //state would grab the json and output it to this template
@@ -21,8 +22,9 @@ class Home extends Component {
             posts.map(post => {
                 return (
                     <div className="post card" key= {post.id}>
+                    <img src={CareerDevs} alt="computer science academy"/>
                     <div className="card-content">
-                    <Link to={'/' + post.id} ><span className="card-title">{post.title}</span></Link>
+                    <Link to={'/' + post.id} ><span className="card-title red-text">{post.title}</span></Link>
                     <p>{post.body}</p>
                     </div>
                     </div>
@@ -32,7 +34,7 @@ class Home extends Component {
             <div className = "center">No posts yet</div>
             );
         return (
-        <div className="container">
+        <div className="container home">
         <h4 className="center">Home</h4>
         {postList}
         </div>
