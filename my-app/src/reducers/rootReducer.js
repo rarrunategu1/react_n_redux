f8696db3ca76e7256171b268a1d5db9339044364
Rose Parker <parker.rose@ymail.com>
2018-10-13 23:24:18 +0000

    a store

--- my-app/src/reducers/rootReducer.js
+++ my-app/src/reducers/rootReducer.js
@@ -1,8 +1,12 @@
 const initState = {
-    posts: []
-}
-    const rootReducer = (state, action) => {
+    posts: [
+        {id: '1', title: 'How CareerDevs changed my life', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum feugiat enim non bibendum. Quisque eget hendrerit felis, eu sollicitudin enim.'},
+        {id: '2', title: 'Code, Code, Code!', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum feugiat enim non bibendum. Quisque eget hendrerit felis, eu sollicitudin enim.'},
+        {id: '3', title: 'My vacation in Bora Bora', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum feugiat enim non bibendum. Quisque eget hendrerit felis, eu sollicitudin enim.'}
+        ]
+};
+    const rootReducer = (state = initState, action) => {
     return state;
-}
+};
 
 export default rootReducer;
