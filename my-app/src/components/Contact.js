28b0c85029ccf59297e3e28246c25e4055b2f20c
Rose Parker <parker.rose@ymail.com>
2018-10-13 16:28:48 +0000

    Used props to redirect the contact page to the about page in 2 second

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const Contact = () => {
+const Contact = (props) => {
+    //redirects contact page to about page in 2 seconds
+    setTimeout(() => {
+        props.history.push('/about');
+    }, 2000); 
     return (
         <div className="container">
         <h4 className="center">Contact</h4>
