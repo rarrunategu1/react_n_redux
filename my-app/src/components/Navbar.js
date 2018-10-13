39523c528b3359de69bbf9c76ffba9bb0be16183
Rose Parker <parker.rose@ymail.com>
2018-10-13 16:30:57 +0000

    Used higher order component withRouter to redirect to the about page in 2 seconds since the props of the navbar were not included in the browser router

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, withRouter } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+    setTimeout(() => {
+        props.history.push('/about');
+    }, 2000);
+    
     return (
         <nav className = "nav-wrapper red darken-3">
             <div className ="container">
-            <a className="brand-logo">React Routing</a>
+            <a href = " " className="brand-logo">React Routing</a>
             <ul className="right">
             <li><Link to="/">Home</Link></li>
             <li><NavLink to="/about">About</NavLink></li>
@@ -16,4 +20,4 @@ const Navbar = () => {
         );
 };
 
-export default Navbar;
+export default withRouter(Navbar);
