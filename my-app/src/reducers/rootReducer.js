39a1ba2652457f589a9938a5e7da5819a31d127a
Rose Parker <parker.rose@ymail.com>
2018-10-14 00:15:59 +0000

    if statement to remove post if action is delete post and return a new state with that id removed

--- my-app/src/reducers/rootReducer.js
+++ my-app/src/reducers/rootReducer.js
@@ -5,8 +5,18 @@ const initState = {
         {id: '3', title: 'My vacation in Bora Bora', body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vestibulum feugiat enim non bibendum. Quisque eget hendrerit felis, eu sollicitudin enim.'}
         ]
 };
+//deletes the post when clicked on and return the new posts
     const rootReducer = (state = initState, action) => {
-    return state;
+        if (action.type === 'DELETE_POST') {
+            let newPosts = state.posts.filter(post => {
+                return action.id !== post.id;
+            });
+            return {
+                ...state,
+                posts: newPosts
+            }
+        }
+    return state; //redirecting the user to the home page after it's updated will happen on the handleClick in post.js
 };
 
 export default rootReducer;
